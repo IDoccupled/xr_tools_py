588da4aec8f2ab20976a52f65f749f6d4dd0f546
xiaoran@labPC <xiaoran.yang@tum.de>
2026-05-16 14:23:23 +0200

    changes

--- print_tools.py
+++ print_tools.py
@@ -1,14 +1,20 @@
 def printgreen(text):
     print(f"\033[92m{text}\033[0m")
-
 def printred(text):
     print(f"\033[91m{text}\033[0m")
-
 def printyellow(text):
     print(f"\033[93m{text}\033[0m")
-
 def printblue(text):
     print(f"\033[94m{text}\033[0m")
+
+def textgreen(text):
+    return f"\033[92m{text}\033[0m"
+def textred(text):
+    return f"\033[91m{text}\033[0m"
+def textyellow(text):
+    return f"\033[93m{text}\033[0m"
+def textblue(text):
+    return f"\033[94m{text}\033[0m"
 
 if __name__ == "__main__":
     printgreen("This is green text.")
