a41e1c7619ae8142545159471caa3ff630749384
xiaoran@labPC <xiaoran.yang@tum.de>
2026-05-16 12:28:07 +0200

    changes

--- print_tools.py
+++ print_tools.py
@@ -6,3 +6,11 @@ def printred(text):
 
 def printyellow(text):
     print(f"\033[93m{text}\033[0m")
+
+def printblue(text):
+    print(f"\033[94m{text}\033[0m")
+
+printgreen("This is green text.")
+printred("This is red text.")
+printyellow("This is yellow text.")
+printblue("This is blue text.")
